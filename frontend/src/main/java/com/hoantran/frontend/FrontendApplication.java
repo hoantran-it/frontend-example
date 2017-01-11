8171eca80b63ccf485e6c5feb8dd805f245eec6f
Hoan Tran <hoantran.it@gmail.com>
2017-01-11 15:02:47 +0700

    Init front-end application

---
+++ frontend/src/main/java/com/hoantran/frontend/FrontendApplication.java
@@ -0,0 +1,18 @@
+/**
+ * Created by Hoan Tran @ http://hoantran-it.blogspot.com
+ *
+ * Any modifications to this file must keep this entire header intact.
+ *
+ */
+package com.hoantran.frontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrontendApplication.class, args);
+    }
+}
